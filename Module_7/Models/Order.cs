a5fabc5125c134d0b94ae3de376ba66fb624f2c6
dvaluev91 <dvaluev91@yandex.ru>
2024-05-15 21:43:13 +0300

    Добавьте файлы проекта.

---
+++ Module_7/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_7.Models
+{
+    internal class Order <TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+        public int Number;
+        public string Description;
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+    }
+}
